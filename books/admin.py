42bd7a2f268d15d281a6fbd7d49159dfece219a4
Mckoy <mckoycandidier@gmail.com>
2017-07-02 20:43:01 +0800

    Make Relational Data of Books and Author Table include code migrations and migrate

--- books/admin.py
+++ books/admin.py
@@ -2,7 +2,8 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import Book
+from .models import Author, Book
 
 # Register your models here.
+admin.site.register(Author)
 admin.site.register(Book)
